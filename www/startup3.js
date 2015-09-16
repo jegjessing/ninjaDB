580818e7a47866f34d1a44023a6fb47a0e2281d8
Niels Liisberg <nli@system-method.com>
2015-09-16 11:40:18 +0200

    Documentation

--- www/startup3.js
+++ www/startup3.js
@@ -3,10 +3,12 @@
 // the "router" is simply serving static files
 // -----------------------------------------------
 
+// Always configure as the very first: 
 ninja.setPort(8080);
 ninja.setWebRoot( "/users/nli/Desktop/ext-6.0.0");
 ninja.setAppRoot( "www");
 
+// Now the path is set for requirement:
 var console = require('console.js');
 console.log("Starting up");
 
